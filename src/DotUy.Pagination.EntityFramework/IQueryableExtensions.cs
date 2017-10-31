638bf76e00e94b842632cde9330d38a34db900a8
Bart calixto <bart@bartmax.com>
2017-10-31 12:37:52 -0300

    add cancellation token support to IQueryable Extensions

--- src/DotUy.Pagination.EntityFramework/IQueryableExtensions.cs
+++ src/DotUy.Pagination.EntityFramework/IQueryableExtensions.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DotUy.Pagination.Extensions
 {
     public static class IQueryableExtensions
     {
-        public async static Task<Page<T>> ToPagedItemsAsync<T>(this IQueryable<T> source, int pageSize, int pageNumber = 1)
+        public async static Task<Page<T>> ToPagedItemsAsync<T>(
+            this IQueryable<T> source, 
+            int pageSize, 
+            int pageNumber = 1, 
+            CancellationToken cancellationToken = default(CancellationToken))
         {
-            var count = await source.CountAsync().ConfigureAwait(false);
+            var count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
             var collection = await source
                 .Skip((pageNumber - 1) * pageSize)
                 .Take(pageSize)
-                .ToArrayAsync().ConfigureAwait(false);
+                .ToArrayAsync(cancellationToken).ConfigureAwait(false);
 
             return new Page<T>(collection, count, pageSize, pageNumber);
         }
